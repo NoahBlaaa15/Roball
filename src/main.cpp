5976da9e661efbddfabd58c01d4dd065fce33b46
Noah Siebart <nosiebart@gmail.com>
2022-12-20 09:15:54 +0100

    motor speed

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,14 @@
 #include "WiFi.h"
 #include "WebSocketsServer.h"
 #include "AsyncMotor.h"
+#include "ArduinoOTA.h"
 
 WebSocketsServer webSocketsServer = WebSocketsServer(81);
 AsyncMotor motorL;
 AsyncMotor motorR;
 
-const u_int8_t speedR = 82;
-const u_int8_t speedL = 90;
+const u_int8_t speedR = 102;
+const u_int8_t speedL = 110;
 
 void webSocketCallback(uint8_t num, WStype_t type, uint8_t * payload, size_t length);
 
@@ -19,6 +20,25 @@ void setup() {
     Serial.begin(9600);
 
     WiFi.softAP("Roball", NULL);
+
+    ArduinoOTA.onStart([]() {
+        Serial.println("Start");
+    });
+    ArduinoOTA.onEnd([]() {
+        Serial.println("\nEnd");
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+    ArduinoOTA.begin();
 
     Serial.println("Opened Access-Point \"Roball\"");
 
@@ -48,6 +68,7 @@ void setup() {
 void loop() {
     //asm("nop");
     webSocketsServer.loop();
+    ArduinoOTA.handle();
 }
 
 void webSocketCallback(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
